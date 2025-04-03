bf549934999a4d4fbff0e7a3060bf83566af89f6
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-04-03 21:04:28 +0300

    GoIT React Home Work №2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Container from "./Container/Container.jsx";
 import Description from "./Description/Description";
 import Feedback from "./Feedback/Feedback";
@@ -5,23 +6,43 @@ import Options from "./Options/Options";
 import Notification from "./Notification/Notification.jsx";
 
 const App = () => {
-  //   const [feedback, setFeedback] = useState({
-  //     good: 0,
-  //     neutral: 0,
-  //     bad: 0,
-  //   });
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleOptionClick = (banan) => {
+    setFeedback((prev) => ({
+      ...prev,
+      [banan]: prev[banan] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = Object.values(feedback).reduce(
+    (acc, item) => acc + item,
+    0
+  );
 
   return (
     <Container>
       <Description />
-      <Options />
-      <Feedback />
-      <Notification />
+      <Options onLeaveFeedback={handleOptionClick} />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          total={totalFeedback}
+          onReset={resetFeedback}
+        />
+      ) : (
+        <Notification message="No feedback given" />
+      )}
     </Container>
   );
-  const updateFeedback = (feedbackType) => {
-    setFeedback({ ...feedback });
-  };
 };
 
 export default App;
